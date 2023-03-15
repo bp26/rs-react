64a45e39c91045605fc348aef26cb0346af40118
bp26 <94074742+bp26@users.noreply.github.com>
2023-03-15 21:53:47 +0300

    feat: update About component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 
 class About extends React.Component {
-  constructor(props) {
+  constructor(props: Record<string, never>) {
     super(props);
   }
 
   render() {
-    return <div className="about"></div>;
+    return (
+      <div className="about">
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium minus a laudantium
+          ipsam nisi sit beatae consectetur unde iusto reiciendis quia commodi libero tempore fuga
+          officiis delectus labore, explicabo mollitia.
+        </p>
+      </div>
+    );
   }
 }
 
