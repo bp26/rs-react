9cd5be14b4b1b007b0dc93eaf59494f50527187f
bp26 <94074742+bp26@users.noreply.github.com>
2023-03-15 21:54:07 +0300

    feat: update ErrorPage component

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
 class ErrorPage extends React.Component {
-  constructor(props) {
+  constructor(props: Record<string, never>) {
     super(props);
   }
 
   render() {
-    return <div className="error"></div>;
+    return (
+      <div className="error">
+        <h1>Error</h1>
+      </div>
+    );
   }
 }
 
