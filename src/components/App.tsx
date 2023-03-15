46c5cfa1e6ac65f99487af52127ced2da31b5398
bp26 <94074742+bp26@users.noreply.github.com>
2023-03-15 21:54:24 +0300

    feat: update App component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Header from './Header/Header';
 
 class App extends React.Component {
-  constructor(props) {
+  constructor(props: Record<string, never>) {
     super(props);
   }
 
   render() {
     return (
       <div className="app">
+        <Header />
         <Outlet />
       </div>
     );
