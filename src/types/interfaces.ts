94b1f20e05125745b0c1f8143daeb613f576dcc2
bp26 <94074742+bp26@users.noreply.github.com>
2023-03-19 20:03:55 +0300

    feat: add interfaces

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -5,3 +5,22 @@ export interface IHeaderState {
 export interface ISearchState {
   query: string;
 }
+
+export interface ICardProps {
+  character: ICharacter;
+}
+
+export interface ICharacter {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  origin: {
+    name: string;
+  };
+  location: {
+    name: string;
+  };
+  image: string;
+  episode: string[];
+}
