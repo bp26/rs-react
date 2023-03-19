f7dd33764791aea8feef692929d7ec4299c494fd
bp26 <94074742+bp26@users.noreply.github.com>
2023-03-19 20:01:15 +0300

    feat: update about componnent

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from './About.module.css';
 
 class About extends React.Component {
   constructor(props: Record<string, never>) {
@@ -7,7 +8,7 @@ class About extends React.Component {
 
   render() {
     return (
-      <div className="about">
+      <div className={[styles.about, 'page'].join(' ')}>
         <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium minus a laudantium
           ipsam nisi sit beatae consectetur unde iusto reiciendis quia commodi libero tempore fuga
