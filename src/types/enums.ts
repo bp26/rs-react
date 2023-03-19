3401f62359acbcfe33ed6c3c68d3315479004989
bp26 <94074742+bp26@users.noreply.github.com>
2023-03-19 20:03:47 +0300

    feat: add enums

--- src/types/enums.ts
+++ src/types/enums.ts
@@ -2,3 +2,13 @@ export const HeaderPageName = {
   MAIN: 'Main',
   ABOUT: 'About',
 };
+
+export const CharacterStatus = {
+  ALIVE: 'Alive',
+  DEAD: 'Dead',
+};
+
+export const CharacterSpecies = {
+  ALIEN: 'Alien',
+  HUMAN: 'Human',
+};
