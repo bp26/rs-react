a4d27d11ec9917f1eddce4dd2c2020dfd1d20226
bp26 <aleshak260006@gmail.com>
2023-03-21 18:51:26 +0000

    feat: add refs to form component

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { LegacyRef } from 'react';
 import { IFormsCard } from 'types/interfaces';
 import styles from './Form.module.css';
 
@@ -7,14 +7,48 @@ interface IFormProps {
 }
 
 class Form extends React.Component<IFormProps> {
+  private firstName = React.createRef<HTMLInputElement>();
+  private lastName = React.createRef<HTMLInputElement>();
+  private email = React.createRef<HTMLInputElement>();
+  private checkbox = React.createRef<HTMLInputElement>();
+
   constructor(props: IFormProps) {
     super(props);
+    this.submit = this.submit.bind(this);
+  }
+
+  private submit(e: React.FormEvent<HTMLInputElement>) {
+    e.preventDefault();
+    const obj = {
+      firstName: this.firstName.current?.value,
+      lastName: this.lastName.current?.value,
+      email: this.email.current?.value,
+      checkbox: this.checkbox.current?.checked,
+    };
+    console.log(obj);
   }
 
   render() {
     return (
       <div className={styles.form}>
-        <span>Form</span>
+        <fieldset>
+          <input type={'text'} ref={this.firstName} placeholder="First name"></input>
+        </fieldset>
+        <fieldset>
+          <input type={'text'} ref={this.lastName} placeholder="Last name"></input>
+        </fieldset>
+        <fieldset>
+          <input type={'email'} ref={this.email} placeholder="Email"></input>
+        </fieldset>
+        <fieldset>
+          <label>
+            <input type={'checkbox'} ref={this.checkbox}></input>
+            Do you consent?
+          </label>
+        </fieldset>
+        <fieldset>
+          <input type={'submit'} onClick={this.submit}></input>
+        </fieldset>
       </div>
     );
   }
