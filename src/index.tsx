286fb1f6df990a3b0d2acd132220d8bee4d89ae7
bp26 <94074742+bp26@users.noreply.github.com>
2023-03-26 23:50:41 +0300

    feat: add alert

--- src/index.tsx
+++ src/index.tsx
@@ -9,3 +9,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
+
+alert(
+  'Уважаемый проверяющий, прошу подождать с проверкой до вторника 28.03! Очень хотелось бы перед проверкой подчистить код и накинуть стилей.\nDear reviewer, please wait with the check until Tuesday 28.03! I would like very much to clean up the code and throw in more styles before the check.'
+);
