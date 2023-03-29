af72e74a735c95320d332c53907f7b4a265875fe
bp26 <aleshak260006@gmail.com>
2023-03-29 07:59:05 +0000

    feat: change ErrorPage to functional component

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -2,18 +2,12 @@ import React from 'react';
 import { joinClassNames } from '../../utils/utils';
 import styles from './ErrorPage.module.css';
 
-class ErrorPage extends React.Component {
-  constructor(props: Record<string, never>) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className={joinClassNames(styles.error, 'page')}>
-        <h1 className={styles.message}>404</h1>
-      </div>
-    );
-  }
-}
+const ErrorPage = () => {
+  return (
+    <div className={joinClassNames(styles.error, 'page')}>
+      <h1 className={styles.message}>404</h1>
+    </div>
+  );
+};
 
 export default ErrorPage;
