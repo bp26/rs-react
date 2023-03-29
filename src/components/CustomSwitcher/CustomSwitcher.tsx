8fd681ecb99ab88988c7d8cbd83cec4ef312923b
bp26 <aleshak260006@gmail.com>
2023-03-29 12:14:59 +0000

    feat: change CustomSwitcher to functional component

--- src/components/CustomSwitcher/CustomSwitcher.tsx
+++ src/components/CustomSwitcher/CustomSwitcher.tsx
@@ -2,42 +2,36 @@ import React from 'react';
 import styles from './CustomSwitcher.module.css';
 import { ISwitcherRef } from 'types/interfaces';
 
-interface CustomSwitcherProps {
+interface ICustomSwitcherProps {
   name: string;
   error: string;
   reference: ISwitcherRef;
   data: { value: string; id: number; ref: string }[];
 }
 
-class CustomSwitcher extends React.Component<CustomSwitcherProps> {
-  constructor(props: CustomSwitcherProps) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <fieldset className={styles.fieldset}>
-        <span className={styles.header}>{this.props.name}</span>
-        <div className={styles.wrapper}>
-          {this.props.data.map((radio) => {
-            return (
-              <label className={styles.label} htmlFor={radio.value} key={radio.id}>
-                <input
-                  className={styles.input}
-                  id={radio.value}
-                  type={'radio'}
-                  name={this.props.name}
-                  ref={this.props.reference[radio.ref as keyof ISwitcherRef]}
-                />
-                {radio.value}
-              </label>
-            );
-          })}
-        </div>
-        <span className={styles.error}>{this.props.error}</span>
-      </fieldset>
-    );
-  }
-}
+const CustomSwitcher = (props: ICustomSwitcherProps) => {
+  return (
+    <fieldset className={styles.fieldset}>
+      <span className={styles.header}>{props.name}</span>
+      <div className={styles.wrapper}>
+        {props.data.map((radio) => {
+          return (
+            <label className={styles.label} htmlFor={radio.value} key={radio.id}>
+              <input
+                className={styles.input}
+                id={radio.value}
+                type={'radio'}
+                name={props.name}
+                ref={props.reference[radio.ref as keyof ISwitcherRef]}
+              />
+              {radio.value}
+            </label>
+          );
+        })}
+      </div>
+      <span className={styles.error}>{props.error}</span>
+    </fieldset>
+  );
+};
 
 export default CustomSwitcher;
