78db59b1ed322dc9c640b58c66ee42b1ae4a4cb4
bp26 <aleshak260006@gmail.com>
2023-03-29 08:05:58 +0000

    feat: change FormsPage to functional component

--- src/pages/FormsPage/FormsPage.tsx
+++ src/pages/FormsPage/FormsPage.tsx
@@ -1,40 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { joinClassNames } from '../../utils/utils';
 import styles from './FormsPage.module.css';
 import Form from '../../components/Form/Form';
 import { IFormsCard } from 'types/interfaces';
 import FormsCard from '../../components/FormsCard/FormsCard';
 
-interface IFormsPageState {
-  cards: IFormsCard[];
-}
+const FormsPage = () => {
+  const [cards, setCards] = useState<IFormsCard[]>([]);
 
-class FormsPage extends React.Component<Record<string, never>, IFormsPageState> {
-  constructor(props: Record<string, never>) {
-    super(props);
-    this.state = {
-      cards: [],
-    };
-  }
+  const createCard = (card: IFormsCard) => {
+    setCards((prevState) => [...prevState, card]);
+  };
 
-  private createCard(card: IFormsCard) {
-    this.setState({ cards: [...this.state.cards, card] });
-  }
-
-  render() {
-    return (
-      <div className={joinClassNames(styles.forms, 'page')}>
-        <div className={styles.formwrapper}>
-          <Form createCard={this.createCard.bind(this)} />
-        </div>
-        <div className={styles.list}>
-          {this.state.cards.map((item, index) => (
-            <FormsCard item={item} key={index} />
-          ))}
-        </div>
+  return (
+    <div className={joinClassNames(styles.forms, 'page')}>
+      <div className={styles.formwrapper}>
+        <Form createCard={createCard} />
       </div>
-    );
-  }
-}
+      <div className={styles.list}>
+        {cards.map((item, index) => (
+          <FormsCard item={item} key={index} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default FormsPage;
