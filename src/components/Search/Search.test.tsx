4b2ce4d5b1d2f30c857f496bcde3ce86e10ee3ab
bp26 <aleshak260006@gmail.com>
2023-03-29 11:56:37 +0000

    feat: add more tests for Search component

--- src/components/Search/Search.test.tsx
+++ src/components/Search/Search.test.tsx
@@ -4,7 +4,11 @@ import { describe, it } from 'vitest';
 import Search from './Search';
 
 describe('Search', () => {
-  it('Saves input value on unmount', () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
+
+  it('Saves input value on unmount and renders it on mount', () => {
     const { unmount } = render(<Search />);
     const input = screen.getByTestId('search-input');
     expect(input).toContainHTML('');
@@ -16,5 +20,17 @@ describe('Search', () => {
     unmount();
     render(<Search />);
     expect(screen.getByTestId('search-input')).toContainHTML('123456');
+  });
+
+  it(`Doesn't save input value to local storage on change`, () => {
+    render(<Search />);
+    const input = screen.getByTestId('search-input');
+    fireEvent.input(input, {
+      target: {
+        value: '123456',
+      },
+    });
+
+    expect(localStorage.getItem('query') === '123456').toBe(false);
   });
 });
