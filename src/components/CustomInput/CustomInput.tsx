2455cf1c9756d5873b17deea7768e426e2fd011e
bp26 <94074742+bp26@users.noreply.github.com>
2023-04-02 16:18:03 +0300

    feat: change CustomInput to suit react-hook-form

--- src/components/CustomInput/CustomInput.tsx
+++ src/components/CustomInput/CustomInput.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import styles from './CustomInput.module.css';
 import { joinClassNames } from '../../utils/utils';
+import { FieldError, UseFormRegisterReturn } from 'react-hook-form';
 
 interface ICustomInputProps {
   type: string;
   name: string;
-  id: string;
-  reference: React.RefObject<HTMLInputElement>;
-  error: string;
+  register: UseFormRegisterReturn;
+  error: FieldError | undefined;
 }
 
 const CustomInput = (props: ICustomInputProps) => {
-  const isInvalid = props.error ? styles.invalid : '';
+  const isInvalid = props?.error ? styles.invalid : '';
   const isTextbox = props.type === 'text' || props.type === 'email' ? styles.textbox : '';
   const isCheckbox = props.type === 'checkbox' ? styles.checkbox : '';
   const isFileUpload = props.type === 'file' ? '' : 'input';
@@ -19,17 +19,14 @@ const CustomInput = (props: ICustomInputProps) => {
   return (
     <fieldset className={joinClassNames(styles.fieldset)}>
       <div className={joinClassNames(styles.wrapper, isCheckbox)}>
-        <label className={styles.label} htmlFor={props.id}>
-          {props.name}
-        </label>
+        <label className={styles.label}>{props.name}</label>
         <input
           className={joinClassNames(styles.input, isTextbox, isInvalid, isFileUpload)}
-          id={props.id}
           type={props.type}
-          ref={props.reference}
+          {...props.register}
         />
       </div>
-      <span className={styles.error}>{props.error}</span>
+      <span className={styles.error}>{props?.error && props.error.message}</span>
     </fieldset>
   );
 };
