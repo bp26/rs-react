e42884124e2d696fe65aaff38f9c35992e4ee482
bp26 <94074742+bp26@users.noreply.github.com>
2023-04-02 16:19:02 +0300

    feat: chnage CustomSwitcher to suit react-hook-form

--- src/components/CustomSwitcher/CustomSwitcher.tsx
+++ src/components/CustomSwitcher/CustomSwitcher.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import styles from './CustomSwitcher.module.css';
-import { ISwitcherRef } from 'types/interfaces';
+import { UseFormRegisterReturn, FieldError } from 'react-hook-form';
 
 interface ICustomSwitcherProps {
   name: string;
-  error: string;
-  reference: ISwitcherRef;
-  data: { value: string; id: number; ref: string }[];
+  error: FieldError | undefined;
+  register: UseFormRegisterReturn;
+  data: { value: string; id: number }[];
 }
 
 const CustomSwitcher = (props: ICustomSwitcherProps) => {
@@ -16,20 +16,19 @@ const CustomSwitcher = (props: ICustomSwitcherProps) => {
       <div className={styles.wrapper}>
         {props.data.map((radio) => {
           return (
-            <label className={styles.label} htmlFor={radio.value} key={radio.id}>
+            <label className={styles.label} key={radio.id} htmlFor={props.register.name}>
               <input
                 className={styles.input}
-                id={radio.value}
                 type={'radio'}
-                name={props.name}
-                ref={props.reference[radio.ref as keyof ISwitcherRef]}
+                {...props.register}
+                value={radio.value}
               />
               {radio.value}
             </label>
           );
         })}
       </div>
-      <span className={styles.error}>{props.error}</span>
+      <span className={styles.error}>{props?.error && props.error.message}</span>
     </fieldset>
   );
 };
