847bedf52c613e14aec53903b9b8f7c49d6d9746
bp26 <94074742+bp26@users.noreply.github.com>
2023-04-02 16:19:30 +0300

    feat: change validation functions

--- src/utils/validation/validation.ts
+++ src/utils/validation/validation.ts
@@ -1,131 +1,90 @@
-import { ValidationError } from '../../types/enums';
-
-export const validateName = (
-  name: string | undefined,
-  setter: (type: string, error: string) => void
-) => {
+export const validateName = (name: string | undefined): string | boolean => {
   if (!name) {
-    setter(ValidationError.NAME, `Full name field shouldn't be empty`);
-    return false;
+    return `Full name field shouldn't be empty`;
   }
 
   const length = name.split(' ').length;
 
   if (length < 2) {
-    setter(ValidationError.NAME, `You should input both your first name and your last name`);
-    return false;
+    return `You should input both your first name and your last name`;
   }
 
   if (length > 2) {
-    setter(ValidationError.NAME, `You shouldn't input more than 2 words`);
-    return false;
+    return `You shouldn't input more than 2 words`;
   }
 
   const [firstName, lastName] = name.split(' ');
 
   if (firstName.length < 3 || lastName.length < 3) {
-    setter(ValidationError.NAME, `First name and last name should be at least 3 character long`);
-    return false;
+    return `First name and last name should be at least 3 character long`;
   }
 
-  setter(ValidationError.NAME, ``);
   return true;
 };
 
-export const validateEmail = (
-  email: string | undefined,
-  setter: (type: string, error: string) => void
-) => {
+export const validateEmail = (email: string | undefined) => {
   if (!email) {
-    setter(ValidationError.EMAIL, `Email field shouldn't be empty`);
-    return false;
+    return `Email field shouldn't be empty`;
   }
 
   if (!email.match(/\S+@\S+\.\S+/)) {
-    setter(ValidationError.EMAIL, `Wrong format of email`);
-    return false;
+    return `Wrong format of email`;
   }
 
-  setter(ValidationError.EMAIL, ``);
   return true;
 };
 
-export const validateBirthday = (
-  dateValue: string | undefined,
-  setter: (type: string, error: string) => void
-) => {
+export const validateBirthday = (dateValue: string | undefined) => {
   if (!dateValue) {
-    setter(ValidationError.BIRTHDAY, `Birthday field should't be empty`);
-    return false;
+    return `Birthday field should't be empty`;
   }
 
   const date = new Date(dateValue);
 
   if (date > new Date()) {
-    setter(ValidationError.BIRTHDAY, `Birthday shouldn't be later than the current date`);
-    return false;
+    return `Birthday shouldn't be later than the current date`;
   }
 
-  setter(ValidationError.BIRTHDAY, ``);
   return true;
 };
 
-export const validateCheckbox = (
-  checked: boolean | undefined,
-  setter: (type: string, error: string) => void
-) => {
+export const validateCheckbox = (checked: boolean | undefined) => {
   if (!checked) {
-    setter(ValidationError.CHECKBOX, `You should consent to having your data processed`);
-    return false;
+    return `You should consent to having your data processed`;
   }
 
-  setter(ValidationError.CHECKBOX, ``);
   return true;
 };
 
-export const validateSelect = (
-  value: string | undefined,
-  setter: (type: string, error: string) => void
-) => {
+export const validateSelect = (value: string | undefined) => {
   if (value === 'Choose an option') {
-    setter(ValidationError.LANGUAGE, `You should choose your language`);
-    return false;
+    return `You should choose your language`;
   }
 
-  setter(ValidationError.LANGUAGE, ``);
   return true;
 };
 
-export const validateSwitcher = (
-  firstOption: boolean | undefined,
-  secondOption: boolean | undefined,
-  setter: (type: string, error: string) => void
-) => {
-  if (!firstOption && !secondOption) {
-    setter(ValidationError.GENDER, `You should choose your gender`);
-    return false;
+export const validateSwitcher = (value: string) => {
+  if (!value) {
+    return `You should choose your gender`;
   }
 
-  setter(ValidationError.GENDER, ``);
   return true;
 };
 
-export const validateFile = (
-  filePath: string | undefined,
-  setter: (type: string, error: string) => void
-) => {
-  if (!filePath) {
-    setter(ValidationError.AVATAR, `You should choose your avatar`);
-    return false;
+export const validateFile = (fileList: FileList) => {
+  const file = fileList[0];
+
+  if (!file) {
+    return `You should choose your avatar`;
   }
 
   const correctExtensions = ['png', 'jpeg', 'jpg', 'bmp'];
-  const ext = filePath.split('.').at(-1);
+  const ext = file.name.split('.').at(-1);
+
   if (ext && !correctExtensions.includes(ext)) {
-    setter(ValidationError.AVATAR, `Wrong image file type`);
-    return false;
+    return `Wrong file type`;
   }
 
-  setter(ValidationError.AVATAR, ``);
   return true;
 };
