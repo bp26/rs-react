7cff115c08cc7d366489657d89a4f5bdb4629f65
bp26 <94074742+bp26@users.noreply.github.com>
2023-04-16 15:03:13 +0300

    feat: update Modal component

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -2,10 +2,16 @@ import React, { PropsWithChildren } from 'react';
 import { createPortal } from 'react-dom';
 import styles from './Modal.module.css';
 
-const Modal = ({ children }: PropsWithChildren) => {
+interface Props {
+  closeModal?: () => void;
+}
+
+const Modal = ({ children, closeModal }: PropsWithChildren<Props>) => {
   return createPortal(
-    <div className={styles.backdrop}>
-      <div className={styles.modal}>{children}</div>
+    <div className={styles.backdrop} onClick={closeModal}>
+      <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
+        {children}
+      </div>
     </div>,
     document.getElementById('modal-root') as HTMLElement
   );
